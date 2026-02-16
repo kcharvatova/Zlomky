4f5cafdddcea2ed6f35a0448b4e96c7312ebdcae
charvka1 <charvka1@uhk.cz>
2026-02-16 12:56:58 +0100

    operace pridane, je potreba dodelat ten override

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,11 +5,43 @@ public class Main {
         IO.println("Projekt ZLOMKY");
 
         var a=new Zlomek();
-        a.setCitatel(3);
-        a.setJmenovatel(4);
+        a.setCitatel(1);
+        a.setJmenovatel(2);
 
         IO.println("Zlomek : "+a.getCitatel()+" / "+a.getJmenovatel());
-        Zlomek b=new Zlomek(4, 5);
+        Zlomek b=new Zlomek(3, 4);
         IO.println(b);
+
+        //operace
+        var soucet=a.plus(b);
+        var rozdil=a.minus(b);
+        var nasobek=a.krat(b);
+        var podil=a.deleno(b);
+
+        IO.println(soucet);
+        IO.println(rozdil);
+        IO.println("Soucet je %s".formatted(soucet.zkratit()));
+
+        Zlomek[] pole=new Zlomek[]{
+                new Zlomek(1,3),
+                new Zlomek(1,2),
+                new Zlomek(6,5),
+                new Zlomek(4,3),
+                new Zlomek(7,8),
+                new Zlomek(13,10)
+        };
+
+        Zlomek bla=prumer(pole);
+        IO.println("Prumer je %s".formatted(bla.zkratit()));
+
+
+    }
+    private static Zlomek prumer(Zlomek[] p){
+        Zlomek soucet=new Zlomek();
+        for(var z:p){
+            soucet=soucet.plus(z);
+        }
+        var pocet=new Zlomek(p.length,1);
+        return soucet.deleno(pocet);
     }
 }
--- src/main/java/cz/uhk/zlomky/Zlomek.java
+++ src/main/java/cz/uhk/zlomky/Zlomek.java
@@ -4,11 +4,12 @@ public class Zlomek {
     private int citatel;
     private int jmenovatel;
 
-    public Zlomek(){
-
+    public Zlomek() {
+        citatel = 0;
+        jmenovatel = 1;
     }
 
-    public Zlomek(int citatel, int jmenovatel){
+    public Zlomek(int citatel, int jmenovatel) {
         this.citatel = citatel;
         this.jmenovatel = jmenovatel;
     }
@@ -29,10 +30,65 @@ public class Zlomek {
         this.jmenovatel = jmenovatel;
     }
 
+    public Zlomek plus(Zlomek druhy) {
+        int cit = citatel * druhy.jmenovatel + druhy.citatel * jmenovatel;
+        int jm = jmenovatel * druhy.jmenovatel;
+        return new Zlomek(cit, jm);
+    }
+
+    public Zlomek plus(int druhy) {
+        int cit = citatel * 1 + druhy * jmenovatel;
+        int jm = jmenovatel;
+        return new Zlomek(cit, jm);
+    }
+    public Zlomek minus(Zlomek druhy) {
+        int cit = citatel * druhy.jmenovatel - druhy.citatel * jmenovatel;
+        int jm = jmenovatel * druhy.jmenovatel;
+        return new Zlomek(cit, jm);
+    }
+
+    public Zlomek minus(int druhy) {
+        int cit = citatel * 1 - druhy * jmenovatel;
+        int jm = jmenovatel;
+        return new Zlomek(cit, jm);
+    }
+
+    public Zlomek krat(Zlomek druhy) {
+        int cit = citatel * druhy.citatel;
+        int jm = jmenovatel * druhy.jmenovatel;
+        return new Zlomek(cit, jm);
+    }
+
+    public Zlomek deleno(Zlomek druhy) {
+        int cit = citatel * druhy.jmenovatel;
+        int jm = jmenovatel * druhy.citatel;
+        return new Zlomek(cit, jm);
+    }
+
+    public Zlomek zkratit() {
+        int delitel = nsd(citatel, jmenovatel); //vypocet nejvetsiho spolecneho delitele
+
+        return new Zlomek(citatel / delitel, jmenovatel / delitel);
+    }
+
+    private int nsd(int a, int b) {
+        if (a < b) {
+            int c = a;
+            a = b;
+            b = c;
+        }
+        int zbytek;
+        do {
+            zbytek = a % b;
+            a = b;
+            b = zbytek;
+        } while (zbytek != 0);
+
+        return a;
+    }
+
     @Override
     public String toString() {
-        return String.format(
-                "%3d\n------\n%3d",
-                citatel, jmenovatel);
-    }
+
+        }
 }
